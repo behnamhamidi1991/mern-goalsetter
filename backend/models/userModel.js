f974bdce6c3893e6d3337aeabd6d7aae0b4cb45b
Behnam <eslp91@hotmail.com>
2024-09-25 00:13:04 +0330

    Last commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
   },
 
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-  },
-
-  password: {
-    type: String,
-    required: [true, "Please add a name"],
-  },
-}
-
-{
-    timestamps: true
-}
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
