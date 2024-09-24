6e9992a682aa48c9b26c1d336a16e12563ed8bd5
behnamhamidi1991 <eslp91@hotmail.com>
2024-09-24 18:50:48 +0330

    new functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get new golas" });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 // @desc Set Goal
@@ -16,7 +20,11 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field!");
   }
 
-  res.status(201).json({ message: "Set a goal" });
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(201).json(goal);
 });
 
 // @desc Update Goals
